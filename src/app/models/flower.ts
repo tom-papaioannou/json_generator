a8f2013c9d87534a65b51759afd30abf8d00e7d1
Tom Papaioannou <Tompap2007@hotmail.com>
2023-12-07 00:15:39 +0200

    Update in UI and add new classes for level

---
+++ src/app/models/flower.ts
@@ -0,0 +1,9 @@
+export class Flower{
+    roots: number;
+    rootTiles: number;
+
+    constructor (x: number) {
+        this.roots = x;
+        this.rootTiles = 5;
+    }
+}
--- src/app/models/level.ts
+++ src/app/models/level.ts
@@ -1,16 +1,28 @@
-export class Level {
-    no: number;
+import { Round } from "./round";
 
-    constructor(no: number) {
-        this.no = no;
+export class Level {
+    level: number;
+    reward: number;
+    rounds: Array<Round>;
+
+    constructor(level: number) {
+        this.level = level;
+        this.reward = 1;
+        // Calculate rounds from level
+        this.rounds = new Array<Round>();
+
+        for(let x = 1; x < (this.level + 1); x++){
+            let round: Round = new Round(x);
+            this.rounds.push(round);
+        }
     }
 
     getLevel() {
         return {
             // Figure those values based on your algorithm
-            no: this.no,
-            roots: 1,
-            rounds: 1
+            level: this.level,
+            reward: this.reward,
+            rounds: this.rounds
         };
     }
 }
